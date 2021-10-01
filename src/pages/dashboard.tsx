5315f61e672f2d0ebd7d79cc0aaa668eb90973bd
flavio <business.flavioferreira@gmail.com>
2021-10-01 12:15:20 -0300

    permissions validate SSR

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,14 @@
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can";
 
 import { AuthContext } from "../contexts/AuthContext";
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut, isAuthenticated } = useContext(AuthContext);
 
   useEffect(() => {
     api.get('/me')
@@ -15,15 +17,21 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign Out</button>
+
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
+    </>
   )
 }
 
 export const getServerSideProps = withSSRAuth(async(ctx) => {
   const apiClient = setupAPIClient(ctx)
   const response = await apiClient.get('/me')
-
-  console.log(response.data)
 
   return {
     props: {}
--- src/utils/withSSRAuth.ts
+++ src/utils/withSSRAuth.ts
@@ -1,11 +1,19 @@
+import decode from "jwt-decode";
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
-import { parseCookies } from "nookies";
+import { destroyCookie, parseCookies } from "nookies";
+import { validateUserPermissions } from "./validateUserPermissions";
 
-export function withSSRAuth<T>(fn: GetServerSideProps<T>): GetServerSideProps {
-  return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<T>> => {
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[]
+}
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>, options?: WithSSRAuthOptions) {
+  return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
+    const token = cookies['nextauth.token']
 
-    if (!cookies['nextauth.token']) {
+    if (!token) {
       return {
         redirect: {
           destination: '/',
@@ -14,6 +22,38 @@ export function withSSRAuth<T>(fn: GetServerSideProps<T>): GetServerSideProps {
       }
     }
 
-    return await fn(ctx);
+    if (options) {
+      const user = decode<{ permissions: string[], roles: string[] }>(token)
+      const { permissions, roles} = options
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles
+      })
+
+      if(!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false
+          }
+        }
+      }
+    }
+
+    try {
+      return await fn(ctx);
+    } catch (err){
+      destroyCookie(ctx, 'nextauth.token')
+      destroyCookie(ctx, 'nextauth.refreshToken')
+
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false
+        }
+      }
+    }
   }
 } 
