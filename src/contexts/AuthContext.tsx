c4c34b50985ced086b971b91a544554920ffe120
flavio <business.flavioferreira@gmail.com>
2021-10-01 12:23:39 -0300

    sign out broadcast channel

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -26,11 +26,15 @@ type AuthProviderProps = {
   children: ReactNode;
 }
 
-export const AuthContext = createContext({} as AuthContextData)
+export const AuthContext = createContext({} as AuthContextData);
+
+let authChannel: BroadcastChannel;
 
 export function signOut(){
   destroyCookie(undefined, 'nextauth.token')
   destroyCookie(undefined, 'nextauth.refreshToken')
+
+  authChannel.postMessage('signOut')
 
   Router.push('/');
 }
@@ -38,6 +42,19 @@ export function signOut(){
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>()
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    authChannel =  new BroadcastChannel('auth')
+    authChannel.onmessage = (message) => {
+      switch (message.data) {
+        case 'signOut':
+          signOut();
+          break;
+        default:
+          break;
+      }
+    }
+  }, [])
 
   useEffect(() => {
     const { 'nextauth.token': token } = parseCookies()
@@ -82,6 +99,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
       api.defaults.headers['Authorization'] = `Bearer ${token}`
 
       Router.push('/dashboard')
+
     } catch (err) {
       console.log(err)
     }
